2f0fbe3ef5b16fa3ee96e9bb71acf2e264565c1c
Moritz Hesche <mo.hesche@gmail.com>
2023-07-27 20:02:50 +0200

    optional git usage

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,24 +1,27 @@
 import subprocess
 
 
-subprocess.run(["git", "init"], check=True)
-subprocess.run(["git", "add", "*"], check=True)
-subprocess.run(
-    ["git", "commit", "-a", "-m", "Initial commit from cookiecutter template"],
-    check=True,
-)
-
-if "{{cookiecutter.remote_url}}":
-    subprocess.run(["git", "checkout", "main"], check=True)
+if "{{cookiecutter.use_git}}" == "yes":
+    subprocess.run(["git", "init"], check=True)
+    subprocess.run(["git", "add", "*"], check=True)
     subprocess.run(
-        ["git", "remote", "add", "origin", "{{cookiecutter.remote_url}}"], check=True
+        ["git", "commit", "-a", "-m", "Initial commit from cookiecutter template"],
+        check=True,
     )
-    subprocess.run(["git", "remote", "-v"], check=True)
-    subprocess.run(["git", "push", "origin", "main"], check=True)
+
+    if "{{cookiecutter.remote_url}}":
+        subprocess.run(["git", "checkout", "main"], check=True)
+        subprocess.run(
+            ["git", "remote", "add", "origin", "{{cookiecutter.remote_url}}"],
+            check=True,
+        )
+        subprocess.run(["git", "remote", "-v"], check=True)
+        subprocess.run(["git", "push", "origin", "main"], check=True)
 
 if "{{cookiecutter.environment}}" == "virtualenv":
     subprocess.run(["python", "-m", "venv", "venv"], check=True)
     subprocess.run(["venv/bin/pip", "install", "-e", ".[dev]"], check=True)
+
 elif "{{cookiecutter.environment}}" == "conda":
     subprocess.run(
         ["conda", "create", "-n", "{{cookiecutter.repo_name}}", "python"],
